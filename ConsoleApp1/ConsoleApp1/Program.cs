1c759e791a5bee3eb832314a3f27649653155bd3
Rupali Natewad <rupalirnatewad@gmail.com>
2022-04-15 14:10:33 +0530

    uploading Singleplyergame file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Snake and Ladder Game");
+snakeladderprb.Singleplayergame.SinglePlayer();
---
+++ ConsoleApp1/ConsoleApp1/Singleplayergame.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeladderprb
+{
+    internal class Singleplayergame
+    {
+        public static int num1, optNum1;
+        public static int position = 0;
+        public static int dicecount = 0;
+
+
+        public static void SinglePlayer()
+        {
+            Console.WriteLine("\n Uc1 Begins here......................................................\n");
+
+            while (position <= 100)
+            {
+                //value for dice
+                Random obj1 = new Random();
+                num1 = obj1.Next(1, 7);
+
+                // value for option
+                Random opt1 = new Random();
+                optNum1 = opt1.Next(1, 4);
+
+                if (optNum1 == 1)  //optNum1=1 ----no play ,,,,, optNum1=2 ----ladder,,,,optNum1=3 ----snake
+                {
+
+                    dicecount++;
+                    Console.WriteLine("You got NO PLAY Option  \n Dice value: {0} \n", num1);
+
+                }
+                else if (optNum1 == 2)
+                {
+                    Console.WriteLine("You got Ladder Option \n  Dice value: {0} \n", num1);
+                    dicecount++;
+                    position = position + num1;
+
+                    if (position > 100)
+                    {
+                        dicecount++;
+                        position = 100;
+                        Console.WriteLine("You got Ladder  \n Dice value: {0} \n", num1);
+                        Console.WriteLine("Here you exceeded position 100 \n");
+                        break;
+                    }
+
+                    Console.WriteLine(" your position: {0}", position);
+
+                }
+                else
+                {
+
+                    dicecount++;
+                    position = position - num1;
+                    Console.WriteLine("Dice value " + num1);
+                    Console.WriteLine("You got Snake Option\n Dice value: {0}\n", num1);
+
+                    if (position < 0)
+                    {
+
+                        position = 0;
+
+                        Console.WriteLine("\n Starting game  again \n");
+
+
+                    }
+                    Console.WriteLine("You got snake \n your position: {0} \n ", position);
+
+                }
+
+
+
+
+            }
+            Console.WriteLine("Number of times dice Roll for winning 100 positin is : {0}", dicecount);
+        }
+
+
+    }
+}
